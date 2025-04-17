2dfa6187202b899424c914df8604f44fe54d46b8
Whofelisberto <felisberto563@gmail.com>
2025-04-17 13:02:19 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// variáveis que usei para poder armazenar os dados.
+
 const input = document.querySelector("input");
 const botao = document.querySelector("button");
 const img = document.querySelector("img");
@@ -5,11 +7,18 @@ const cidade = document.querySelector("#cidade");
 const grau = document.querySelector("#grau");
 const conteudo = document.querySelector(".content");
 
-botao.addEventListener("click", () => {
-  if(!input.value) return;
-  getWeatherData(); 
-});
+// quando clicar no botão do campo para pesquisar , essa função vai retornar os dados da api.
 
+botao.addEventListener("click", () => {
+  if(!input.value) {
+    return alert("Insira um lugar, por favor!")
+  } else {
+    getWeatherData();
+  }
+  });
+  
+// função que pega as informações do clima tempo fazendo um fetch na api e retornando os dados que pedi .
+  
 async function getWeatherData() {
   let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input.value)}&units=metric&appid=a8cc3e20ab42161b0ebeff582448f875`
       try{
@@ -26,7 +35,8 @@ async function getWeatherData() {
       }
 }
 
-
+// função que carrega as informação da api, fazendo o conteudo aparecer ja que estava em display none.
+  
 function loadWeatherInfo(data) {
   cidade.innerHTML = `${data.name}, ${data.sys.country}`;
   grau.innerHTML = `Temperatura: ${Math.floor(data.main.temp)} ° C`;
